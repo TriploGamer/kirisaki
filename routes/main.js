a7a55bfdd484335f4d0c806d8b166b45168bd3e3
JG-BOTSOFC <116314156+JG-BOTSOFC@users.noreply.github.com>
2022-10-25 15:46:19 -0300

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -3,15 +3,26 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/valor', (req, res) => {
-res.sendFile(__path + '/views/valor.html')
-})
 router.get('/', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 router.get('/api', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
+router.get('/log', (req, res) => {
+res.sendFile(__path + '/views/logos-txt.html')
+})
+router.get('/out', (req, res) => {
+res.sendFile(__path + '/views/outros.html')
+})
+/////////////////////////////////////////////
+router.get('/fun', (req, res) => {
+res.sendFile(__path + '/views/funcoes.html')
+})
+router.get('/dow', (req, res) => {
+res.sendFile(__path + '/views/download.html')
+})
+/////////////////////////////////////////////
 
 router.get('/config', (req, res) => {
     config = {
